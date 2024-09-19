840cbe3e8c64cbb69d8f6e6998489102a8fa4674
Vipin Devan <vipindevan08@gmail.com>
2024-09-19 21:12:33 +0200

    ssss:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
-import { Link } from "react-scroll";
-import { BiRestaurant } from "react-icons/bi";
-import Button from "../layouts/Button";
-import { AiOutlineMenuUnfold } from "react-icons/ai";
-import { BiChevronDown } from "react-icons/bi";
-import { AiOutlineClose } from "react-icons/ai";
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import { BiRestaurant } from 'react-icons/bi';
+import Button from '../layouts/Button';
+import { AiOutlineMenuUnfold } from 'react-icons/ai';
+import { BiChevronDown } from 'react-icons/bi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../assets/img/logo123.jpeg';
 
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
@@ -22,32 +23,18 @@ const Navbar = () => {
       <div>
         <div className=" flex flex-row justify-between p-5 md:px-32 px-5 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
           <div className=" flex flex-row items-center cursor-pointer">
-            <span>
-              <BiRestaurant size={32} />
-            </span>
-            <h1 className=" text-xl font-semibold">FoodieWeb</h1>
+            <h1 className=" text-xl font-semibold"></h1>
+            <img className=" w-20" src={logo} alt="img" />
           </div>
 
           <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8">
-            <Link
-              to="home"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="hover:text-brightColor transition-all cursor-pointer"
-            >
+            <Link to="home" spy={true} smooth={true} duration={500} className="hover:text-brightColor transition-all cursor-pointer">
               Home
             </Link>
 
             <div className="relative group">
               <div className=" flex items-center gap-1">
-                <Link
-                  to="dishes"
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                  className="hover:text-brightColor transition-all cursor-pointer"
-                >
+                <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-brightColor transition-all cursor-pointer">
                   Dishes
                 </Link>
 
@@ -102,33 +89,15 @@ const Navbar = () => {
               </ul>
             </div>
 
-            <Link
-              to="about"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="hover:text-brightColor transition-all cursor-pointer"
-            >
+            <Link to="about" spy={true} smooth={true} duration={500} className="hover:text-brightColor transition-all cursor-pointer">
               About
             </Link>
 
-            <Link
-              to="menu"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="hover:text-brightColor transition-all cursor-pointer"
-            >
+            <Link to="menu" spy={true} smooth={true} duration={500} className="hover:text-brightColor transition-all cursor-pointer">
               Menu
             </Link>
 
-            <Link
-              to="review"
-              spy={true}
-              smooth={true}
-              duration={500}
-              className="hover:text-brightColor transition-all cursor-pointer"
-            >
+            <Link to="review" spy={true} smooth={true} duration={500} className="hover:text-brightColor transition-all cursor-pointer">
               Reviews
             </Link>
 
@@ -136,16 +105,12 @@ const Navbar = () => {
           </nav>
 
           <div className="md:hidden flex items-center">
-            {menu ? (
-              <AiOutlineClose size={25} onClick={handleChange} />
-            ) : (
-              <AiOutlineMenuUnfold size={25} onClick={handleChange} />
-            )}
+            {menu ? <AiOutlineClose size={25} onClick={handleChange} /> : <AiOutlineMenuUnfold size={25} onClick={handleChange} />}
           </div>
         </div>
         <div
           className={` ${
-            menu ? "translate-x-0" : "-translate-x-full"
+            menu ? 'translate-x-0' : '-translate-x-full'
           } lg:hidden flex flex-col absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}
         >
           <Link
