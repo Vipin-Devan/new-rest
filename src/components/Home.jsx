af964b240dc1a2f1a7c3a5f6285b3d4a9817049a
Vipin Devan <vipindevan08@gmail.com>
2024-09-19 22:12:55 +0200

    ss

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,12 @@ const Home = () => {
   return (
     <div className=" min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/img/heroo11.jpg')] bg-cover bg-no-repeat ">
       <div className=" w-full lg:w-2/3 space-y-5">
+        <h1 className="text-orange-300 font-semibold text-6xl">For a break that’s truly fulfilling</h1>
+
+        <p className=" text-backgroundColor">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis et qui, maxime assumenda repellat corrupti officia dolorum delectus labore
+          del
+        </p>
         <div className=" lg:pl-44">
           <Button title="Order Now" />
         </div>
